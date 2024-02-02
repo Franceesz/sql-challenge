0b01fbf6196fe7118d4eda071dc673bd41a44143
Frances Zhu <148593376+Franceesz@users.noreply.github.com>
2024-02-02 15:59:12 -0600

    Adjustment

--- EmployeeSQL/employees_queries.sql
+++ EmployeeSQL/employees_queries.sql
@@ -1,7 +1,8 @@
 -- employee number, last name, first name, sex, and salary
 SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 FROM employees e
-JOIN salaries s
+--Adjust to left join to ensure that employees without salary information are retained in results  
+LEFT JOIN salaries s 
 ON e.emp_no = s.emp_no;
 
 -- first name, last name, and hire date for employees hired in 1986
@@ -13,18 +14,18 @@ ORDER BY hire_date ASC;
 -- managers in each department (dept number, dept name, emp number, last name, first name)
 SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name 
 FROM dept_manager dm
-JOIN employees e
+INNER JOIN employees e
 ON dm.emp_no = e.emp_no
-JOIN departments d
+INNER JOIN departments d
 ON dm.dept_no = d.dept_no
 ORDER BY d.dept_name ASC;
 
 -- department of each employee (emp number, last name, first name, dept name)
 SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
 FROM employees e
-JOIN dept_emp de 
+INNER JOIN dept_emp de 
 ON e.emp_no = de.emp_no
-JOIN departments d
+INNER JOIN departments d
 ON d.dept_no = de.dept_no
 ORDER BY d.dept_name ASC;
 
@@ -37,9 +38,9 @@ ORDER BY last_name ASC;
 -- employees in Sales department (emp no, last name, first name, dept name)
 SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
 FROM employees e
-JOIN dept_emp de 
+INNER JOIN dept_emp de 
 ON e.emp_no = de.emp_no
-JOIN departments d
+INNER JOIN departments d
 ON d.dept_no = de.dept_no
 WHERE d.dept_name = 'Sales';
 
